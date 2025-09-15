fa4670dae5a5d66dec8e70ef1de431b92a87012c
1100109226 <1100109226@L815WD220016.edu.cbe.ab.ca>
2025-09-15 14:03:33 -0600

    2:03pm

---
+++ Chapterr3/src/Assignment1/Exercise1.java
@@ -0,0 +1,29 @@
+
+
+
+
+package Assignment1;
+
+import java.util.Scanner;
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		int height = 100;
+		int time = (int)4.5;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a time less than 4.5 seconds: ");
+		time = input.nextInt();
+		input.close();
+		
+		int height = 100 - (int)4.9
+		
+		
+		
+		
+		
+	}
+
+}
--- Chapterr3/src/Skillbuilders/RectanglePerimeter.java
+++ Chapterr3/src/Skillbuilders/RectanglePerimeter.java
@@ -4,6 +4,13 @@ public class RectanglePerimeter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int length = 13;
+		int width = 4;
+		
+		int perimeter = 2 *(width + length);
+		
+		System.out.println("The perimeter is: " + perimeter);
 
 	}
 
