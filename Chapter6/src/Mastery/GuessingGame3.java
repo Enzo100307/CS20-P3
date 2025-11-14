db966dcc461f992790ba42cbafe7bbe335c8b7c8
1100109226 <1100109226@L815WD220016.edu.cbe.ab.ca>
2025-11-14 11:41:37 -0700

    11:41pm

---
+++ Chapter6/src/Mastery/GuessingGame3.java
@@ -0,0 +1,21 @@
+package Mastery;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessingGame3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	 
+	    
+	    	
+	    
+		
+		
+
+	}
+
+}
