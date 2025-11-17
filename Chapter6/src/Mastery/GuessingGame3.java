ac47cc215d726df33df5b2433e05ead22c3a1720
1100109226 <1100109226@L815WD220016.edu.cbe.ab.ca>
2025-11-17 14:01:20 -0700

    2:01pm

--- Chapter6/src/Mastery/GuessingGame3.java
+++ Chapter6/src/Mastery/GuessingGame3.java
@@ -8,6 +8,44 @@ public class GuessingGame3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int secretNum;
+		int userNum;
+		int tries = 0;
+		
+		Random random = new Random();
+		Scanner input = new Scanner(System.in);
+		
+		secretNum = random.nextInt(20) + 1;
+		
+		do
+		{
+			System.out.println("Enter a number between 1 and 20: ");
+			userNum = input.nextInt();
+			tries++;
+			
+			if (secretNum == userNum) {
+				System.out.println("You won!");
+			} else if (secretNum >= userNum) {
+				System.out.println("Hint: try a higher number");
+			} else if (secretNum <= userNum) {
+				System.out.println("Hint: try a lower number");
+			} else {
+				System.out.println("Invalid input!");
+			}
+				
+		} 
+		while (secretNum != userNum);
+		
+		input.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	 
 	    
