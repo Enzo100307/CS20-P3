86938a19c359bf6e773be05b9e5e7bbbf6549360
1100109226 <1100109226@L815WD220016.edu.cbe.ab.ca>
2025-12-08 13:39:06 -0700

    1:38pm

---
+++ Chapter9/src/Skillbuilders/RandomStats.java
@@ -0,0 +1,27 @@
+package Skillbuilders;
+
+public class RandomStats {
+
+	public static void main(String[] args) 
+	{
+		int[] outcomes = new int[10];
+		int outcome;
+		
+		//Store values into the outcomes Array
+		
+		for(int i = 0; i < 100; i++)
+		{
+			outcome = (int)(10*Math.random());
+			outcomes[outcome] += 1;
+			
+		}
+		
+		//Display what is in the Array
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.println(outcomes[i]);
+		}
+		
+	}
+
+}
